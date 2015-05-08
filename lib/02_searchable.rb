b94a0d3409677a0326e63264c22b557bbdff465c
thelaird <hey@thisis.lt>
2015-05-08 15:07:08 -0700

    Is searchable

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,21 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_string = params.keys.map { |key| "#{key} = ?"}
+    where_string = where_string.join(' AND ')
+    data = DBConnection.execute(<<-SQL,*params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_string}
+      SQL
+
+    self.parse_all(data)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
