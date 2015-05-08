cf0a65359f8a8b324b3bd06af8e6581a1fa59be7
thelaird <hey@thisis.lt>
2015-05-08 11:12:38 -0700

    Adds my_attr_accessor

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,12 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method("#{name}") do
+        name
+      end
+      define_method("#{name}=") do |val|
+        name =  val
+      end
+    end
   end
 end
