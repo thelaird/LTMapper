a88dd732f7db5b1f22dc5dd27cc3360e4ce208bc
thelaird <hey@thisis.lt>
2015-05-08 17:19:17 -0700

    Completes belongs_to and has_many

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,41 +10,62 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @name = name
+    @foreign_key = options[:foreign_key] || "#{name.to_s}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @name = name
+    @foreign_key = options[:foreign_key] || "#{self_class_name.downcase.to_s}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      foreign_key = self.class.assoc_options[name].foreign_key
+      target_model_class = self.class.assoc_options[name].model_class
+
+      data = target_model_class
+             .where(self.class.assoc_options[name].primary_key => self.send(foreign_key))
+      data.first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+    define_method(name) do
+      foreign_key = options.foreign_key
+      primary_key = options.primary_key
+      target_model_class = options.model_class
+      target_model_class.where(foreign_key => self.send(primary_key))
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,6 +5,7 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    through_options = assoc_options[through_name]
+    source_options = through_options.assoc_options[source_name]
   end
 end
